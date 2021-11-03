611741889ff1249b24c079d8eee291a0b6a6a4b7
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2021-11-03 12:18:55 -0300

    Criando Menu

---
+++ Projeto C4/Assets/Scripts/Button/Button.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button:MonoBehaviour{
+    public void StartGame(){
+		
+	}
+	
+	public void OptionGame(){
+		
+	}
+	
+	public void CloseGame(){
+		
+	}
+}
