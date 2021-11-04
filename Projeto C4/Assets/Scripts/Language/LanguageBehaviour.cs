7916179381695ba771a3a3b7ac1ac3ebfaea6c4f
GabrielFrigo <gabriel.frigo4@gmail.com>
2021-11-03 22:52:09 -0300

    Duas línguas

--- Projeto C4/Assets/Scripts/Language/LanguageBehaviour.cs
+++ Projeto C4/Assets/Scripts/Language/LanguageBehaviour.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class LanguageBehaviour : MonoBehaviour
 {
+    public static Lingua lingua = Lingua.Portugues;
+    [SerializeField] string portugues, ingles;
+    [SerializeField] Text text;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetLenguage();
     }
 
     // Update is called once per frame
@@ -15,4 +20,18 @@ public class LanguageBehaviour : MonoBehaviour
     {
         
     }
+
+    public void SetLenguage()
+    {
+        if (lingua == Lingua.Portugues)
+        {
+            text.text = portugues;
+        }
+    }
+}
+
+public enum Lingua
+{
+    Portugues = 0,
+    Ingles = 1,
 }
