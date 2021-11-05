4a37390eae986346c9a85c4bf05e66e0ce1375d9
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2021-11-05 09:35:38 -0300

    Create Audio System

---
+++ Projeto C4/Assets/Scripts/GUI/SliderScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SliderScript : MonoBehaviour
+{
+	public static float volumeMusic = 0.5f, volumeSound = 0.5f;
+	
+    public void VolumeMusic(float volume)
+	{
+		volumeMusic = volume;
+	}
+	
+	public void VolumeSound(float volume)
+	{
+		volumeSound = volume;
+	}
+}
