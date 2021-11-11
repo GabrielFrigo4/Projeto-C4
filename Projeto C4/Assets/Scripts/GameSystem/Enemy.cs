3b7b5f9bbac7d8762b938125c6b1682bf13f787f
GabrielFrigo <gabriel.frigo4@gmail.com>
2021-11-11 20:10:22 -0300

    update path

--- Projeto C4/Assets/Scripts/GameSystem/Enemy.cs
+++ Projeto C4/Assets/Scripts/GameSystem/Enemy.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static CodeUtils;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public List<Vector2> path = new List<Vector2>();
+	Vector2 randomPos;
+	float destroySelfDistance = 0.5f;
+
+	// Start is called before the first frame update
+	void Start()
     {
-        
-    }
+		randomPos = new Vector2(Random.Range(-0.35f, 0.35f), Random.Range(-0.35f, 0.35f));
+		destroySelfDistance = Random.Range(0.35f, 0.65f);
+	}
 
     // Update is called once per frame
     void Update()
     {
-        
+		Vector2 targetPosition = path[0] * 2 + new Vector2(-15f, -8f) + randomPos;
+		Vector2 moveDir = (targetPosition - (Vector2)transform.position).normalized;
+
+		float moveSpeed = 6f;
+
+		transform.position += (Vector3)moveDir * moveSpeed * Time.deltaTime;
+
+		float angle = GetAngleFromVector(moveDir);
+		transform.eulerAngles = new Vector3(0, 0, angle);
+
+		if (Vector2.Distance(transform.position, targetPosition) < destroySelfDistance)
+		{
+			if (path.Count > 1)
+				path.RemoveAt(0);
+			else
+				PassedOn();
+		}
+	}
+
+	public void PassedOn()
+    {
+		Destroy(gameObject);
     }
 }
--- Projeto C4/Assets/Scripts/GameSystem/GameIA.cs
+++ Projeto C4/Assets/Scripts/GameSystem/GameIA.cs
@@ -7,93 +7,123 @@ using static CodeUtils;
 public class GameIA : MonoBehaviour
 {
 	[SerializeField]Tilemap map;
-	[SerializeField]List<Vector3> starts, ends;
+	[SerializeField]List<Vector2> starts, indentationStarts;
 	[SerializeField]List<TileBase> tileNoone;
 	[SerializeField]List<TileBase> tileGround;
 	[SerializeField]List<TileBase> tilePath;
 	[SerializeField]List<TileBase> tileTowerPositon;
-	public List<Vector3> paths = new List<Vector3>();
+	public List<List<Vector2>> paths = new List<List<Vector2>>();
 	Grid grid;
+
+	int time = 10;
 	
     void Start()
     {
         grid = new Grid(16, 8, 2f, transform.position);
 		UpdateGridToTilemapValue();
-		GetPath(paths);
+		paths.Add(new List<Vector2>());
+		GetPath(paths[0]);
+		SpawnEnemy(paths[0]);
     }
 
     void Update()
     {
-        if(Input.GetMouseButtonDown(1))
+		time--;
+		if(time == 0)
+        {
+			SpawnEnemy(paths[0]);
+			time = 10;
+		}
+		if (Input.GetMouseButtonDown(1))
 		{
 			SpawnTower();
 		}
     }
 	
+	void GetPath(List<Vector2> path)
+	{	
+		void GetNextPosition(Vector2 position, Vector2 direction)
+		{
+			path.Add(position);
+			Vector2 up, down, left, right;
+			up = position + Vector2.up;
+			down = position + Vector2.down;
+			left = position + Vector2.left;
+			right = position + Vector2.right;
+			
+			if(grid.GetValue((int)up.x, (int)up.y) == GridType.path && !path.Contains(up))
+			{
+				GetNextPosition(up, Vector2.up);
+			}
+			else if(grid.GetValue((int)down.x, (int)down.y) == GridType.path && !path.Contains(down))
+			{
+				GetNextPosition(down, Vector2.down);
+			}
+			else if(grid.GetValue((int)left.x, (int)left.y) == GridType.path && !path.Contains(left))
+			{
+				GetNextPosition(left, Vector2.left);
+			}
+			else if(grid.GetValue((int)right.x, (int)right.y) == GridType.path && !path.Contains(right))
+			{
+				GetNextPosition(right, Vector2.right);
+			}
+            else
+            {
+				path.Add(position + direction);
+			}
+		}
+		
+		GetNextPosition(starts[0], Vector2.zero);
+	}
+
+	void SpawnEnemy(List<Vector2> path)
+    {
+		Vector3 createPosition = starts[0] * 2 + new Vector2(-15f, -8f) + indentationStarts[0] * 2;
+		GameObject inimigo = Instantiate((GameObject)Resources.Load("Prefab/Enemy"), createPosition, Quaternion.identity);
+		Enemy en = inimigo.GetComponent<Enemy>();
+
+		List<Vector2> arrayVar = new List<Vector2>();
+
+		path.ForEach((item) =>
+		{
+			arrayVar.Add(item);
+		});
+
+		en.path = arrayVar;
+	}
+
 	void UpdateGridToTilemapValue()
 	{
-		for(int x = 0; x < 18; x++)
+		for (int x = 0; x < 18; x++)
 		{
-			for(int y = 0; y < 8; y++)
+			for (int y = 0; y < 8; y++)
 			{
-				TileBase b = map.GetTile(new Vector3Int(x,y,0));
-				
-				if(b == null)
+				TileBase b = map.GetTile(new Vector3Int(x, y, 0));
+
+				if (b == null)
 				{
 					grid.SetValue(x, y, GridType.outside);
 				}
-				else if(tileNoone.Contains(b))
+				else if (tileNoone.Contains(b))
 				{
 					grid.SetValue(x, y, GridType.noone);
 				}
-				else if(tileGround.Contains(b))
+				else if (tileGround.Contains(b))
 				{
 					grid.SetValue(x, y, GridType.ground);
 				}
-				else if(tilePath.Contains(b))
+				else if (tilePath.Contains(b))
 				{
 					grid.SetValue(x, y, GridType.path);
 				}
-				else if(tileTowerPositon.Contains(b))
+				else if (tileTowerPositon.Contains(b))
 				{
 					grid.SetValue(x, y, GridType.towerPosition);
 				}
 			}
 		}
 	}
-	
-	void GetPath(List<Vector3> path)
-	{	
-		void GetNextPosition(Vector3 position)
-		{
-			path.Add(position);
-			Vector3 up, down, left, right;
-			up = position + Vector3.up;
-			down = position + Vector3.down;
-			left = position + Vector3.left;
-			right = position + Vector3.right;
-			
-			if(grid.GetValue((int)up.x, (int)up.y) == GridType.path && !path.Contains(up))
-			{
-				GetNextPosition(up);
-			}
-			else if(grid.GetValue((int)down.x, (int)down.y) == GridType.path && !path.Contains(down))
-			{
-				GetNextPosition(down);
-			}
-			else if(grid.GetValue((int)left.x, (int)left.y) == GridType.path && !path.Contains(left))
-			{
-				GetNextPosition(left);
-			}
-			else if(grid.GetValue((int)right.x, (int)right.y) == GridType.path && !path.Contains(right))
-			{
-				GetNextPosition(right);
-			}			
-		}
-		
-		GetNextPosition(starts[0]);
-	}
-	
+
 	void SpawnTower()
 	{
 		Vector3 spawnPosition = GetMouseWorldPosition();
