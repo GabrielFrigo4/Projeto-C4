8b7efa00e897120116b129e731631cb788886704
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2021-11-17 10:38:37 -0300

    Update

--- Projeto C4/Assets/Scripts/GameSystem/Tower.cs
+++ Projeto C4/Assets/Scripts/GameSystem/Tower.cs
@@ -5,11 +5,18 @@ using static CodeUtils;
 
 public class Tower : MonoBehaviour
 {
+	public TowerType type;
 	Vector3 projectileShootFromPositon;
+	GameObject rangeObj;
 	
     void Awake()
 	{
 		projectileShootFromPositon = transform.Find("projectileShootFromPositon").position;
+	}
+	
+	void Start()
+	{
+		rangeObj = gameObject.transform.Find("distancia").gameObject;
 	}
 	
 	void Update()
@@ -18,5 +25,7 @@ public class Tower : MonoBehaviour
 		{
 			Projectile.Create(projectileShootFromPositon, GetMouseWorldPosition());
 		}
+		
+		rangeObj.transform.localScale = new Vector3(type.range, type.range, 1);
 	}
 }
---
+++ Projeto C4/Assets/Scripts/ScriptableObject/Torre/TowerType.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TowerType", menuName = "ScriptableObjects/TowerType")]
+public class TowerType : ScriptableObject
+{
+	public float range, time;
+}
