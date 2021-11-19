854c61a36c7af52ded325652f2d022ccb99ccadf
GabrielFrigo <gabriel.frigo4@gmail.com>
2021-11-19 19:40:35 -0300

    Update Logo

---
+++ Projeto C4/Assets/Scripts/Other/LogoAnimation.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogoAnimation : MonoBehaviour
+{
+	[SerializeField] Texture2D logo;
+	private IEnumerator coroutine;
+	private SpriteRenderer spriteRender;
+    const int SIZE = 116;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRender = GetComponent<SpriteRenderer>();
+        coroutine = AnimationPlay(0.095f);
+        StartCoroutine(coroutine);
+    }
+	
+	IEnumerator AnimationPlay(float timeSecondFrame)
+	{
+		for(int i = 0; i < 81; i++)
+        {
+            spriteRender.sprite = Sprite.Create(logo, new Rect(SIZE * i, 0, SIZE, SIZE), new Vector2(0.5f, 0.5f), 7, 1);
+            yield return new WaitForSeconds(timeSecondFrame);
+        }
+        SceneScript.GoScene("Menu");
+	}
+}
--- Projeto C4/Assets/Scripts/Sound/SoundPlay.cs
+++ Projeto C4/Assets/Scripts/Sound/SoundPlay.cs
@@ -9,7 +9,7 @@ public class SoundPlay : MonoBehaviour
 {
 	[SerializeField] AudioClip clip;
 	[Range(0, 1)] [SerializeField] float volume = 1f;
-	[SerializeField] bool isStatic = false;
+	[SerializeField] bool isStatic = false, playOnStart = true, loop = false;
 	[SerializeField] SoundVolumeType typeSound;
 
 	static GameObject clipSound;
@@ -28,7 +28,7 @@ public class SoundPlay : MonoBehaviour
 	
     void Start()
     {
-		Play();
+		if(playOnStart) Play();
     }
 
     public void Play()
@@ -36,18 +36,18 @@ public class SoundPlay : MonoBehaviour
         switch (typeSound)
         {
 			case SoundVolumeType.Normal:
-				PlayClip(clip, GetPointerPtr(ref volume), isStatic);
+				PlayClip(clip, GetPointerPtr(ref volume), isStatic, loop);
 				break;
 			case SoundVolumeType.Music:
-				PlayClip(clip, GetPointerPtr(ref SliderScript.volumeMusic), isStatic);
+				PlayClip(clip, GetPointerPtr(ref SliderScript.volumeMusic), isStatic, loop);
 				break;
 			case SoundVolumeType.Sound:
-				PlayClip(clip, GetPointerPtr(ref SliderScript.volumeSound), isStatic);
+				PlayClip(clip, GetPointerPtr(ref SliderScript.volumeSound), isStatic, loop);
 				break;
 		}
 	}
 	
-	public static GameObject PlayClip(AudioClip clip, IntPtr volume, bool isStatic)
+	public static GameObject PlayClip(AudioClip clip, IntPtr volume, bool isStatic, bool loop)
 	{
 		GameObject sound = Instantiate(ClipSound);
 		ClipPlayScript clipScript = sound.GetComponent<ClipPlayScript>();
