1ed120aaad5fb3244492164ce5bc6421a42d2292
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2021-11-22 13:13:41 -0300

    Update

--- Projeto C4/Assets/Scripts/GUI/ButtonBehaviour.cs
+++ Projeto C4/Assets/Scripts/GUI/ButtonBehaviour.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static CodeUtils;
 
 public class ButtonBehaviour : MonoBehaviour
 {
+	//[SerializeField] Sprite Normal, Highlighted, Pressed;
+	[SerializeField] float size;
+	
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,15 @@ public class ButtonBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+		/*
+        if(size < GetDistance2D(tranform.position, GetMouseWorldPosition()))
+		{
+			transform.localSize = new Vector3(1.1f, 1.1f, 1f);
+		}
+		else
+		{
+			transform.localSize = new Vector3(1f, 1f, 1f);
+		}
+		*/
     }
 }
