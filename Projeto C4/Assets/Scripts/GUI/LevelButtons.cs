3e9c3412e6ace60a8b7e550a040e6eaf81aa2686
bot-real <victorsda2005@gmail.com>
2021-11-29 19:08:19 -0300

    asda

--- Projeto C4/Assets/Scripts/GUI/LevelButtons.cs
+++ Projeto C4/Assets/Scripts/GUI/LevelButtons.cs
@@ -8,10 +8,12 @@ public class LevelButtons : MonoBehaviour
 	public Button[] buttons;
 	public static int currentLevel = 0;
 	public static bool canGo2Next = false;
+	Transform leveltext;
 
 	void Start()
     {
 		buttons = GetComponentsInChildren<Button>();
+		leveltext = transform.GetChild(0);
 		RefreshButtons();
 	}
 
@@ -22,11 +24,12 @@ public class LevelButtons : MonoBehaviour
 			if (i > currentLevel)
 			{
 				buttons[i].interactable = false;
+				transform.GetChild(i).transform.GetChild(0).gameObject.SetActive(false);
 			}
 			else if (i == currentLevel)
 			{
 				buttons[i].interactable = true;
-			
+				transform.GetChild(i).transform.GetChild(0).gameObject.SetActive(true);
 			}
 			else if (i < currentLevel)
 			{
